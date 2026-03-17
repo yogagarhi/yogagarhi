bad9c574c3d392a88bea50585f598319f0b8d009
yogagarhi <yogagarhi@gmail.com>
2026-03-18 00:55:07 +0530

    new changes in new branch

--- next-frontend/src/app/robots.ts
+++ next-frontend/src/app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/api/', '/_next/', '/private/'],
+            },
+        ],
         sitemap: 'https://www.yogagarhi.com/sitemap.xml',
     }
 }
+
